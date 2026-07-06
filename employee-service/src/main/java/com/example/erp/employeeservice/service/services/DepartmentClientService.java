a7647d22c70cc4c0a490e83fdd8b035f40376059
abolfazl <abolfazlhamidian9@gmail.com>
2026-07-06 20:03:50 +0330

    feature(postman): fix employee and departments bug for dockerfile

--- employee-service/src/main/java/com/example/erp/employeeservice/service/services/DepartmentClientService.java
+++ employee-service/src/main/java/com/example/erp/employeeservice/service/services/DepartmentClientService.java
@@ -1,10 +1,11 @@
 package com.example.erp.employeeservice.service.services;
 
 import com.example.erp.employeeservice.service.contracts.IDepartmentClientService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
-
+@Service
 public class DepartmentClientService implements IDepartmentClientService
 {
     private final WebClient webClient;
